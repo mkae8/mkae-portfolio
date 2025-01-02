6cae8ad7eab59198d5ea57b17540b150ed5f4286
24LP9087 24LP9087 <ntemka93@gmail.com>
2025-01-02 12:59:10 +0800

    fix

--- client/app/components/test.tsx
+++
@@ -1,7 +0,0 @@
-const createInnovation = () => {
-  const skills = ["Problem Solving", "Clean Code", "Scalability"];
-
-  return skills.reduce((acc, skill) => {
-    return acc * implement(skill);
-  }, 1);
-};
